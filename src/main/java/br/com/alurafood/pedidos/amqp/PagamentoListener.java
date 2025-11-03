c5c98071373a79ceec59067053d1d93654d3dbc2
Jhack Rychard De Oliveira Costa <jhack.costa@sankhya.com.br>
2025-11-03 00:36:35 -0300

    feat: consome msg como PagamentoDTO

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,15 @@ public class PagamentoListener {
 
 
     @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message message) {
-        System.out.println("recebi a mensagem " + message.toString());
+    public void recebeMensagem(PagamentoDto pagamentoDto) {
+        String mensagem = """
+               Dados do pagamento: %s
+               Número do pedido: %s
+               Nome: %s
+               Valor R$: %s
+               Status: %s 
+           """.formatted(pagamentoDto.getId(), pagamentoDto.getPedidoId(), pagamentoDto.getNome(),  pagamentoDto.getValor(), pagamentoDto.getStatus());
+
+        System.out.println("Recebi a mensagem " + mensagem);
     }
 }
